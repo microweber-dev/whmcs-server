8097ea3f2e43f4406586786dd400e44255fb2f50
b.slaveykov <b.slaveykov@radioactive.bg>
2020-01-03 18:43:37 +0200

    update

--- modules/addons/microweber_server/microweber_server.php
+++ modules/addons/microweber_server/microweber_server.php
@@ -30,12 +30,31 @@ function microweber_server_clientarea($vars)
 
 function microweber_server_output($vars)
 {
+    $products = array();
+    $results = localAPI('GetProducts', array());
+    if (isset($results['products']['product'])) {
+        foreach ($results['products']['product'] as $product) {
+            $products[] = $product;
+        }
+    }
+
     $html = "<h3>Mark plans wich is a Whitelabel Enterprise</h3>";
-    $html .= "<br>";
 
+    $html = '<form method="post">';
 
-    $html .= "<button type='button' class='btn btn-success'>Save</button>";
+    foreach ($products as $product) {
+
+        $html .= '<label style="padding: 5px;border: 1px solid #00000021;margin-right: 5px;">';
+        $html .= '<input type="checkbox" value="'.$product['pid'].'" name="product_ids[]">';
+        $html .= ' ' . $product['name'];
+        $html .= '</label>';
+        $html .= '<br />';
+
+    }
+
+    $html .= "<button type='submit' class='btn btn-success'>Save</button>";
     $html .= '<hr>';
+    $html .= '</form>';
 
     echo $html;
 }
