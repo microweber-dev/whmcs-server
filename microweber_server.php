be0619abba1f34a6a0ad729862bdad7dd482db0d
b.slaveykov <b.slaveykov@radioactive.bg>
2020-01-06 16:00:26 +0200

    update

--- microweber_server.php
+++ microweber_server.php
@@ -1,4 +1,7 @@
 <?php
+
+use WHMCS\Database\Capsule;
+
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
@@ -26,39 +29,90 @@ function microweber_server_config()
 
 function microweber_server_clientarea($vars)
 {
+    $params = array();
 
+    if ($_GET) {
+        $params = array_merge($params, $_GET);
+    }
+
+    if ($_POST) {
+        $params = array_merge($params, $_POST);
+    }
+
+    $response = $vars;
+
+    $controller = new \MicroweberServer\ApiController();
+    $method = false;
+    if (isset($_GET['function'])) {
+        $method = $_GET['function'];
+    }
+    if (method_exists($controller, $method)) {
+        $response = $controller->$method($params);
+    }
+
+    if ($response) {
+        echo json_encode($response, JSON_PRETTY_PRINT);
+        exit;
+    }
 }
 
 function microweber_server_output($vars)
 {
     $response = '';
     $params = array();
+
     if ($_GET) {
         $params = array_merge($params, $_GET);
     }
+
     if ($_POST) {
         $params = array_merge($params, $_POST);
     }
+
     if ($vars) {
         $params = array_merge($params, $vars);
     }
+
     $controller = new \MicroweberServer\AdminController();
     $method = 'index';
+
     if (isset($params['function'])) {
         $method = $params['function'];
     }
+
     if (method_exists($controller, $method)) {
         $response = $controller->$method($params);
     }
+
     echo $response;
 }
 
 function microweber_server_activate()
 {
-
+    try {
+        if (!Capsule::schema()->hasTable('mod_microweber_usage_reports')) {
+            Capsule::schema()->create(
+                'mod_microweber_usage_reports',
+                function ($table) {
+                    $table->increments('id');
+                    $table->string('domain');
+                    $table->string('server_ip');
+                    $table->string('license_key');
+                    $table->integer('clients_total');
+                    $table->timestamps();
+                }
+            );
+        }
+    } catch (\Exception $e) {
+        echo "Unable to create mod_microweber_usage_reports: {$e->getMessage()}";
+    }
 }
 
 function microweber_server_deactivate()
 {
-
+    try {
+        Capsule::schema()->dropIfExists('mod_microweber_usage_reports');
+    } catch (\Exception $e) {
+        echo "Unable to drop table mod_microweber_usage_reports: {$e->getMessage()}";
+    }
 }
---
+++ src/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MicroweberServer;
+
+use WHMCS\Database\Capsule;
+
+class ApiController
+{
+    public function save_usage_report()
+    {
+
+        
+        echo 111;
+        die();
+    }
+}
