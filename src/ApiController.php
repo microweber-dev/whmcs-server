67583cc4134bc455985def553d23aef1374ad4fd
b.slaveykov <b.slaveykov@radioactive.bg>
2020-01-06 16:58:07 +0200

    update

--- src/ApiController.php
+++ src/ApiController.php
@@ -13,6 +13,8 @@ class ApiController
 
             $totalClients = (int)$_POST['total_clients'];
             $whmcsDomain = $_POST['whmcs_domain'];
+            $serverIp = $_POST['server_ip'];
+            $licenseKey = $_POST['license_key'];
 
             $reportCheck = Capsule::table('mod_microweber_usage_reports')->where(['domain' => $whmcsDomain])->first();
             if ($reportCheck) {
@@ -21,6 +23,8 @@ class ApiController
                     ->where('id', $reportCheck->id)
                     ->update(
                         [
+                            'server_ip' => $serverIp,
+                            'license_key'=> $licenseKey,
                             'total_clients' => $totalClients,
                             'updated_at' => date("Y-m-d H:i:s")
                         ]
@@ -28,6 +32,8 @@ class ApiController
             } else {
                 // Save
                 Capsule::table('mod_microweber_usage_reports')->insert([
+                    'server_ip' => $serverIp,
+                    'license_key'=> $licenseKey,
                     'domain' => $whmcsDomain,
                     'total_clients' => $totalClients,
                     'created_at' => date("Y-m-d H:i:s"),
