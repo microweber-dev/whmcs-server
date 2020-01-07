29e88b32436280aef49fa77e6d5d9720c98cc5c6
b.slaveykov <b.slaveykov@radioactive.bg>
2020-01-07 17:50:30 +0200

    update

--- src/ApiController.php
+++ src/ApiController.php
@@ -7,9 +7,59 @@ use WHMCS\Database\Capsule;
 class ApiController
 {
 
-    public function validate_api_key()
+    public function create_account()
     {
 
+        if (!isset($_GET['api_key']) && !isset($_GET['domain']) && !isset($_GET['username']) && !isset($_GET['password'])) {
+            return false;
+        }
+
+        if (empty($_GET['api_key']) || empty($_GET['domain']) || empty($_GET['username']) || empty($_GET['password'])) {
+            return false;
+        }
+
+        $domain = $_GET['domain'];
+        $api_key = $_GET['api_key'];
+        $get_api_key = Capsule::table('mod_microweber_cloudconnect_api_keys')
+            ->where('api_key_type', 'default')
+            ->where('api_key', $api_key)->first();
+
+        $product_id = 1;
+        $client_id = $get_api_key->client_id;
+
+        $orderData = array(
+            'clientid' => $client_id,
+            'pid' => array($product_id),
+            'domain' => array($domain),
+            'billingcycle' => array('monthly'),
+           // 'configoptions' => array(base64_encode(serialize(array("1" => 999))), base64_encode(serialize(array("1" => 999)))),
+            //'domaintype' => array('register', 'register'),
+            'regperiod' => array(1, 2),
+            'noinvoiceemail'=>true,
+            'noemail' => true,
+            'paymentmethod' => 'mailin',
+            'dnsmanagement' => array(0 => false, 1 => true),
+        );
+        $addOrder = localAPI('AddOrder', $orderData);
+        if (isset($addOrder['orderid'])) {
+
+            $acceptOrderData = array(
+                'orderid' => $addOrder['orderid'],
+                'autosetup' => true,
+                'sendemail' => false,
+            );
+
+            var_dump($addOrder);
+
+            $acceptOrder = localAPI('AcceptOrder', $acceptOrderData);
+
+            print_r($acceptOrder);
+
+        }
+    }
+
+    public function validate_api_key()
+    {
         if (isset($_GET['api_key'])) {
             $api_key = $_GET['api_key'];
 
